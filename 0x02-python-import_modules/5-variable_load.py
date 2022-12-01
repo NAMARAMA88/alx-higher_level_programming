60c8a94879931b9d7531d69b602d577ffb0cc2ff
NAMARAMA88 <coulrama2@gmail.com>
2022-12-01 08:38:20 -0800

    5-variable_load.py

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,10 +1,4 @@
 #!/usr/bin/python3
-from variable_load_5 import a
-
-
-def find_var():
-    print(a)
-
-
-if __name__ == "__main__":
-    find_var()
+if __name__ == '__main__':
+        from variable_load_5 import a
+        print(a)
