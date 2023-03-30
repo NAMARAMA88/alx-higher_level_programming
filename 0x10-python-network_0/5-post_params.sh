b941c2e3237c27c041f2d4f65dd2ea3aa522d406
NAMARAMA88 <coulrama2@gmail.com>
2023-03-30 08:26:39 -0700

    5-post_params.sh
    <
    >
    
    chmod u+x 5-post_params.sh

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# takes in a URL, sends a POST request to the passed URL, and displays the body of the response
-curl -sd "email=hr@holbertonschool.com&subject=I%20will%20always%20be%20here%20for%20PLD" "$1"
+#takes in a URL, sends a POST request to the passed URL,and displays the body of the response
+curl -s -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
